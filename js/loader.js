9a53f11d241e35c3baba505d7aea83052fe58668
AnkitBiswas1991 <ankit.biswas@codeclouds.in>
2026-06-15 18:07:30 +0530

    updated database

--- js/loader.js
+++ js/loader.js
@@ -3,7 +3,7 @@ export function loader(parentEl){
         <div class="position-absolute loader-wrapper top-0 bottom-0 start-0 end-0">
             <div class="loader position-absolute top-50 start-50 translate-middle d-flex align-items-center justify-content-center">
                 <figure>
-                    <img src="../images/loader-img.png" alt="loader">
+                    <img src="./images/loader-img.png" alt="loader">
                 </figure>
             </div>
         </div>
